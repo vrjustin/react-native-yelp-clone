cbc8defde26a0c5dc4747cad4481be7ccfd1cc09
Justin Maronde <justin.maronde@cengage.com>
2021-06-14 07:30:33 -0400

    Updated the style of the SearchBar component to start matching the specs. added margin, background color, and other basic styling.
    import and start using the SearchBar from the SearchScreen.
    
    Updated styling temporarily on the SearchScreen to make easier to see ther SearchBar for now.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,11 +2,20 @@ import React from 'react';
 import { View , Text, StyleSheet } from 'react-native'
 
 const SearchBar = () => {
-    return <View>
-        <Text>Search Bar</Text>
-    </View>
+    return (
+        <View style={styles.background} >
+            <Text>Search Bar</Text>
+        </View>
+    )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    background: {
+        backgroundColor: '#F0EEEE',
+        height: 50,
+        borderRadius: 5,
+        marginHorizontal: 15
+    }
+});
 
 export default SearchBar;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { View , Text, StyleSheet } from 'react-native'
+import { View , Text, StyleSheet } from 'react-native';
+import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
-    return <View>
-        <Text>Search Screen</Text>
-    </View>
+    return (
+        <View style={styles.background} >
+            <SearchBar />
+            <Text>Search Screen</Text>
+        </View>
+    )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    background: {
+        backgroundColor: 'white'
+    }
+});
 
 export default SearchScreen;
