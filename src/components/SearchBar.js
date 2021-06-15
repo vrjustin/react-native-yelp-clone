c9a388e3a06ba1cd53d1abffe8c86820e099c3f4
Justin Maronde <justin.maronde@cengage.com>
2021-06-15 07:10:41 -0400

    Search screen is the component that needs to manage the state - even though the state is being modified by the TextInput child component. So we need to useState from that parent component.
    then you pass the state variable as well as the callback function ‘setTerm’ that we defined to the TextInput - inside the SearchBar
    
    ** attempted to use the autoCapitalize prop on the TextInput and it should be preventing the first character from capitalize but it isn’t working!.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { View , TextInput, StyleSheet } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange }) => {
     return (
         <View style={styles.backgroundStyle} >
             <Feather name="search" style={styles.iconStyle} />
             <TextInput style={styles.inputStyle}
+                autoCapitalize="none"
+                autoCorrect={false}
                 placeholder="Search" 
+                value={term}
+                onChangeText={newTerm => onTermChange(newTerm)}
             />
         </View>
-    )
+    );
 };
 
 const styles = StyleSheet.create({
