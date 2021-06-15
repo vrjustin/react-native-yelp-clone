28bf8763faf519a56ec0065dbfabf875bd874e65
Justin Maronde <justin.maronde@cengage.com>
2021-06-15 06:54:58 -0400

    refactor the Feather icon styling from inline styling defining and passing style props.
    remove previous border on inputStyle - it was just there to help us figure out layout - specs do not want a border.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,7 +5,7 @@ import { Feather } from '@expo/vector-icons'
 const SearchBar = () => {
     return (
         <View style={styles.backgroundStyle} >
-            <Feather name="search" size={30} />
+            <Feather name="search" style={styles.iconStyle} />
             <TextInput style={styles.inputStyle}
                 placeholder="Search" 
             />
@@ -15,6 +15,7 @@ const SearchBar = () => {
 
 const styles = StyleSheet.create({
     backgroundStyle: {
+        marginTop: 10,
         backgroundColor: '#F0EEEE',
         height: 50,
         borderRadius: 5,
@@ -22,9 +23,13 @@ const styles = StyleSheet.create({
         flexDirection: 'row'
     },
     inputStyle: {
-        borderColor: 'black',
-        borderWidth: 1,
-        flex: 1
+        flex: 1,
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center',
+        marginHorizontal: 15
     }
 });
 
