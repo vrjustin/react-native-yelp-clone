1124ba869dfb183e3150f8b4ff90f8754a000a34
Justin Maronde <justin.maronde@cengage.com>
2021-06-18 16:18:32 -0400

    add new boiler plater ResultsShowScreen and import so we will be able to use it in our stackNavigator. Add new key for this new component.
    add console.log for the props object. - just so we can see what the props object contains. this is so we can eventually recall we use the navigation.navigate prop..

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -4,7 +4,8 @@ import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList';
 
-const SearchScreen = () => {
+const SearchScreen = (props) => {
+    console.log(props);
     const [term, setTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults();
 
