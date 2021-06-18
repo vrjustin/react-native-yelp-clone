565f17f1c9d29bd9b3be7e37959bd61f19b1d8b4
Justin Maronde <justin.maronde@cengage.com>
2021-06-18 06:56:34 -0400

    Add in additional styling to the ResultsList container and title. Add in additional styling to the ResultsDetail container & Image.

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -4,7 +4,7 @@ import ResultsDetail from './ResultsDetail';
 
 const ResultsList = ({ title, results }) => {
     return(
-        <View>
+        <View style={styles.container}>
             <Text style={styles.titleStyle}>{title}</Text>
             <FlatList 
                 horizontal={true}
@@ -21,7 +21,12 @@ const ResultsList = ({ title, results }) => {
 const styles = StyleSheet.create({
     titleStyle: {
         fontSize: 18,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        marginLeft: 15,
+        marginBottom: 5
+    },
+    container: {
+        marginBottom: 10
     }
 });
 
