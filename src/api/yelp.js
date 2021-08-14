fcc94dc94d28d1112087f1a9ba64504367426784
Justin Maronde <justin.maronde@cengage.com>
2021-08-14 06:54:47 -0400

    remove our hardcoded api key. move to a file in a secret directory and gitignore that secret dir.

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
+import apiKey from "../../secret/yelpToken.json";
 
+const assembledKey = "Bearer `${apiKey}`";
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer zhjqTiCSBFcOrEh9wv6-kW_qMEALeGrdFS722WIhzVJgedHbAj8HgYI3vuyCZQlUkGz8lMJ9klaPeSurJFQdiQZwDN47m_BA0t__sUbEDrMRnS0dUc2ZRRVT5kjDYHYx'
+        Authorization: apiKey.key
     }
 });
